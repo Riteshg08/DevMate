c12549e4e22421ebd38a6b75a153941876167feb
Ritesh Gurkhe <riteshgurkhe@gmail.com>
2026-03-14 15:31:39 +0530

    Refactor error handling in getData routes and add middleware for centralized error management

--- src/App.js
+++ src/App.js
@@ -4,29 +4,32 @@ const express = require('express');
 // app is used to define routes, middleware(code that runs between the request and response), and server config
 const app = express();
 
-const {authAdmin,authUser} = require("./middlewares/auth.js");
-
-//handling the auth using middelwears
-app.use("/admin",authAdmin);
-
-//for admin data we are using authAdmin middleware to check if the admin is authorized or not before sending the response
-//if the admin is not authorized then it will send the response as unauthorized and if it is authorized then it will send the response as admin data
-app.get("/admin/getData",(req,res) =>{
-    res.send("This is the admin data");
+//This route handler handle the error in the route and send the response to the client
+app.get("/getData", (req, res) => {
+    try {
+        throw new Error("Something went wrong!!!");
+        res.send("Data is fetched successfully");
+    }
+    catch (err) {
+        res.status(500).send("Error:Please contact to administrator");;
+    }
 });
 
-app.delete("/admin/deleteData",(req,res) =>{
-    res.send("The admin data is deleted");
+
+//Error is not handle in the route so it will reach to the error handling middleware and send the response to the client
+app.get("/getData/123", (req, res) => {
+
+    throw new Error("Something went wrong!!!");
+    res.send("Data is fetched successfully");
 });
 
-//for user login there is no need of auth as it is the login page, so we are not using any middleware here
-app.use("/user/login",(req,res)=>{
-    res.send("This is the user login page");
-});
 
-//for user data we are using authUser middleware to check if the user is authorized or not
-app.use("user/data",authUser,(req,res)=>{
-    res.send("This is the user data");
+//error handling middleware means it will catch the error thrown in the above route and send the response to the client
+// if the error is handle in the route then it will not reach to the error handling middleware and send the response to the client
+app.use("/", (err, req, res, next) => {
+    if (err) {
+        res.status(500).send("Something went wrong");
+    }
 });
 
 
