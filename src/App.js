608975d2b149b2242968d54e053398809fbd53ca
Ritesh Gurkhe <riteshgurkhe@gmail.com>
2026-03-15 17:39:18 +0530

    Accepting the dynamic user for the signup route

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ const app = express();
 
 const User = require("./models/user");
 
+//middleware to parse the incoming request body as JSON
+//express.json() converts JSON request into js object so we can access it
+app.use(express.json());
+
 //we use async because saving data in the database is asynchronous operation and we need to wait for it to complete before sending the response to the client
 app.post("/signup", async (req, res) => {
     //creating a new instance of the user model
-    const user = new User({
-        firstName: "MS",
-        lastName: "Dhoni",
-        email: "ms.dhoni@example.com",
-        age: 44,
-        gender: "Male",
-        password: "dhoni123"
-    });
+    console.log(req.body);
+    
+    //pushing a dynamic data given by the client in the request body to the user model
+    const user = new User(req.body);
 
     //saving the user to the database
     try {
@@ -25,12 +25,15 @@ app.post("/signup", async (req, res) => {
         res.status(400).send("Error:" + err.message);
     }
 });
-
+ 
 
 connectDB()
     .then(() => {
         console.log("Database is connected successfully!");
         app.listen(7777, () => {
+
+
+
             console.log("Server is created successfully!");
         });
     })
